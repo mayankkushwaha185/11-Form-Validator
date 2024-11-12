a23e473a027548b13c725b675553b83b6e76f0c7
Mayank Kushwaha <mayankKushwaha185@gmail.com>
2024-11-12 22:24:57 +0530

    form validator

---
+++ script.js
@@ -0,0 +1,71 @@
+const form = document.getElementById('form')
+const password1El = document.getElementById('password1')
+const password2El = document.getElementById('password2')
+const messageContainer = document.querySelector('.message-container')
+const message = document.getElementById('message')
+
+
+let isValid = false;
+let passwordsMatch = false
+function validateForm(){
+    // using constrant Api
+    isValid =form.checkValidity()
+    // style main Message for an error
+    if(!isValid){
+        message.textContent = 'Please fill out all fields'
+        message.style.color = 'red'
+        messageContainer.style.borderColor ='red'
+        return;
+    }
+    // check to see if passwords match
+    if(password1El.value === password2El.value){
+        passwordsMatch = true;
+        password1El.style.color = 'green'
+        password2El.style.borderColor = 'green'
+    }else{
+        passwordsMatch = false
+        message.textContent = 'Make sure Password Match.'
+        message.style.color = 'red'
+        messageContainer.style.borderColor= 'red'
+        password1El.style.color = 'red'
+        password2El.style.borderColor = 'red'
+        return
+
+    }
+    // If form is valid and passwords match
+    if(isValid && passwordsMatch){
+        message.textContent= 'Successfully Registered'
+        message.style.color = 'green'
+        messageContainer.style.borderColor= 'green'
+    }
+
+}
+
+function storeFormData(){
+    const user ={
+        name: form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        website: form.website.value,
+        password: form.password.value
+    }
+    // do Something with user Data
+    console.log(user)
+}
+
+
+
+
+
+function processFormData(e){
+    e.preventDefault();
+    // Validate Form
+    validateForm()
+    // Submit data If valid
+    if(isValid && passwordsMatch ){
+        storeFormData()
+    }
+}
+
+// Event Listener
+form.addEventListener('submit', processFormData)
